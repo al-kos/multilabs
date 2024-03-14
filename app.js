fc40a15b4fbdde25dd5c975e154e10919eee5ba6
Alex Kos <kosenkoaleksey34@gmail.com>
2024-03-14 07:50:22 +0300

    create IO

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const readline = require('node:readline')
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+rl.question('Введите первое число: ', function (num1) {
+  rl.question('Введите второе число: ', function (num2) {
+    let sum = Number(num1) + Number(num2)
+    console.log('Сумма чисел равна: ' + sum)
+    rl.close()
+  })
+  rl.on('error', function (error) {
+    console.log('Ошибка при чтении ввода: ' + error)
+    rl.close()
+  })
+})
