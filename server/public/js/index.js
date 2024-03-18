f60ab9992d4b9d910cfb99fbc333e5cd04aa69b3
Alex Kos <kosenkoaleksey34@gmail.com>
2024-03-18 12:16:07 +0300

    edit UI

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,55 +1,83 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   // Обработка событий формы генерации массива
-  document.getElementById('user_data').addEventListener('submit', (event) => {
-    event.preventDefault()
+  document
+    .getElementById('user_data')
+    .addEventListener('submit', async (event) => {
+      event.preventDefault()
 
-    // Получение значений полей ввода
-    const lenLimit = document.getElementById('lenLimit').value
-    const minLimit = document.getElementById('minLimit').value
-    const maxLimit = document.getElementById('maxLimit').value
+      // Получение значений полей ввода
+      const lenLimit = document.getElementById('lenLimit').value
+      const minLimit = document.getElementById('minLimit').value
+      const maxLimit = document.getElementById('maxLimit').value
 
-    const userData = {
-      lenLimit: Number(lenLimit),
-      minLimit: Number(minLimit),
-      maxLimit: Number(maxLimit),
-    }
-    console.log(userData)
-    // Отправка объекта на сервер
-    fetch('http://localhost:5000', {
-      method: 'POST',
-      body: JSON.stringify(userData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      const userData = {
+        lenLimit: Number(lenLimit),
+        minLimit: Number(minLimit),
+        maxLimit: Number(maxLimit),
+      }
+      console.log(userData)
+      // Отправка объекта на сервер
+      const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await response.json()
+      console.log('Success:', data)
+      document.getElementById('result').innerText = JSON.stringify(
+        data.randomArray,
+      )
     })
-  })
   // Обработка событий формы сортировки массива
-  document.getElementById('sorting').addEventListener('submit', (event) => {
-    event.preventDefault()
+  document
+    .getElementById('sorting')
+    .addEventListener('submit', async (event) => {
+      event.preventDefault()
 
-    // Получение выбранного метода сортировки
-    const sortType = document.querySelector(
-      'input[name="sort_type"]:checked',
-    ).value
+      // Получение выбранного метода сортировки
+      const sortType = document.querySelector(
+        'input[name="sort_type"]:checked',
+      ).value
 
-    // Сортировка массива
-    switch (sortType) {
-      case 'selectionSort':
-        console.log('Выбран тип сортировки, появления результата сортировки')
-        console.log(sortType)
-        // array.selectionSort()
-        break
-      case 'insertionSort':
-        // array.insertionSort()
-        break
-      case 'quickSort':
-        // array.quickSort()
-        break
-      case 'bubbleSort':
-        // array.bubbleSort()
-        break
-    }
-  })
+      let user_selection_sort
+
+      // Сортировка массива
+      switch (sortType) {
+        case 'selectionSort':
+          user_selection_sort = sortType
+          // array.selectionSort()
+          break
+        case 'insertionSort':
+          user_selection_sort = sortType
+          // array.insertionSort()
+          break
+        case 'quickSort':
+          user_selection_sort = sortType
+          // array.quickSort()
+          break
+        case 'bubbleSort':
+          user_selection_sort = sortType
+          // array.bubbleSort()
+          break
+      }
+
+      console.log(user_selection_sort)
+      // Отправка объекта на сервер
+      const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        body: JSON.stringify(user_selection_sort),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await response.json()
+      console.log('Success:', data)
+      // document.getElementById('result').innerText = JSON.stringify(
+      //   data.randomArray,
+      // )
+    })
   // Обработка событий формы поиска элемента
   document.getElementById('searching').addEventListener('submit', (event) => {
     event.preventDefault()
