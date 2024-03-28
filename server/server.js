880deacd4c55d660125cc3f774fbfaf12095d806
Alex Kos <kosenkoaleksey34@gmail.com>
2024-03-28 16:29:39 +0300

    broke it myself fixed it myself

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // require('dotenv').config()
+// const path = require('path')
 
 const RandomArrayGenerator = require('../classes/RandomArrayGenerator')
 const Sorting = require('../classes/Sorting')
@@ -6,32 +7,28 @@ const Searching = require('../classes/Searching')
 const Monitoring = require('../classes/Monitoring')
 
 const express = require('express')
-const expressHbs = require('express-handlebars')
-const path = require('path')
-const hbs = require('hbs')
-const router = express.Router()
+const { engine } = require('express-handlebars')
+const handlebars = require('handlebars')
 const bodyParser = require('body-parser')
 
 const app = express()
 
-app.set('view engine', 'hbs')
+app.engine(
+  'handlebars',
+  engine({
+    defaultLayout: 'main',
+  }),
+)
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(express.json())
 app.use(express.static('public'))
 
-// app.use('/', (req, res) => {
-//   res.render('main.hbs')
-// })
+app.get('/', (req, res) => res.render('root'))
 
-app.get('/', (req, res) => {
-  res.sendFile('/views/layouts/layout.html', { root: 'public' })
-})
-
-app.get('/algs', (req, res) => {
-  res.sendFile('/views/partials/algoritms.html', { root: 'public' })
-})
+app.get('/algs', (req, res) => res.render('algs'))
 
 app.listen(5000, () => {
   console.log('Сервер запущен, порт 5000')
