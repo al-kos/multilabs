819b163009fbff95209124904edc903bab15197d
Alex Kos <kosenkoaleksey34@gmail.com>
2024-03-28 21:29:06 +0300

    add new bodyparser

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,86 +1,77 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Обработка событий формы генерации массива
-  document
-    .getElementById('user_data')
-    .addEventListener('submit', async (event) => {
-      event.preventDefault()
-      let lenLimit = null
-      let minLimit = null
-      let maxLimit = null
-      // Получение значений полей ввода
-      lenLimit = document.getElementById('lenLimit').value
-      minLimit = document.getElementById('minLimit').value
-      maxLimit = document.getElementById('maxLimit').value
+const user_data_for_array = document.getElementById('user_data')
+const user_sorting_select = document.getElementById('sorting')
+const user_searching_select = document.getElementById('searching')
 
-      const userData = {
+// Обработка событий формы генерации массива после загрузки html
+document.addEventListener('DOMContentLoaded', () => {
+  user_data_for_array.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    let lenLimit = document.getElementById('lenLimit').value
+    let minLimit = document.getElementById('minLimit').value
+    let maxLimit = document.getElementById('maxLimit').value
+    console.log(lenLimit, minLimit, maxLimit)
+    // Отправка объекта на сервер
+    const response = await fetch('http://localhost:5000/algs', {
+      method: 'POST',
+      body: JSON.stringify({
         lenLimit: Number(lenLimit),
         minLimit: Number(minLimit),
         maxLimit: Number(maxLimit),
-      }
-      // Отправка объекта на сервер
-      const response = await fetch('http://localhost:5000', {
-        method: 'POST',
-        body: JSON.stringify(userData),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      const data = await response.json()
-      console.log(data.randomArray)
-      // document.getElementById('result').innerText = JSON.stringify(
-      //   data.randomArray,
-      // )
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
+    const data = await response.json()
+    console.log(data)
+    // document.getElementById('result').innerText = JSON.stringify(
+    //   data.randomArray,
+    // )
+  })
   // Обработка событий формы сортировки массива
-  document
-    .getElementById('sorting')
-    .addEventListener('submit', async (event) => {
-      event.preventDefault()
+  user_sorting_select.addEventListener('submit', async (event) => {
+    event.preventDefault()
 
-      // Получение выбранного метода сортировки
-      let userSortData = {
+    // Отправка объекта на сервер
+    const response = await fetch('http://localhost:5000/algs', {
+      method: 'POST',
+      body: JSON.stringify({
+        // Получение выбранного метода сортировки
         user_selection_sort: document.querySelector(
           'input[name="sort_type"]:checked',
         ).value,
-      }
-      // Отправка объекта на сервер
-      const response = await fetch('http://localhost:5000', {
-        method: 'POST',
-        body: JSON.stringify(userSortData),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      //!!!!!!!!!!!!!! OUT RESULT NEED ADD EMPTY DIV WITH ID
-      // const data = await response.json()
-      // document.getElementById('result').innerText = JSON.stringify(
-      //   data.randomArray,
-      // )
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
+    //!!!!!!!!!!!!!! OUT RESULT NEED ADD EMPTY DIV WITH ID
+    // const data = await response.json()
+    // document.getElementById('result').innerText = JSON.stringify(
+    //   data.randomArray,
+    // )
+  })
   // Обработка событий формы поиска элемента
-  document
-    .getElementById('searching')
-    .addEventListener('submit', async (event) => {
-      event.preventDefault()
+  user_searching_select.addEventListener('submit', async (event) => {
+    event.preventDefault()
 
-      let userSearchData = {
+    // Отправка объекта на сервер
+    const response = await fetch('http://localhost:5000/algs', {
+      method: 'POST',
+      body: JSON.stringify({
         // Получение значения элемента для поиска
         searchElement: document.getElementById('search_element').value,
         // Получение выбранного метода поиска
         user_selection_search: document.getElementById('search_type').value,
-      }
-      // Отправка объекта на сервер
-      const response = await fetch('http://localhost:5000', {
-        method: 'POST',
-        body: JSON.stringify(userSearchData),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-
-      // const data = await response.json()
-      // document.getElementById('result').innerText = JSON.stringify(
-      //   data.randomArray,
-      // )
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
+
+    // const data = await response.json()
+    // document.getElementById('result').innerText = JSON.stringify(
+    //   data.randomArray,
+    // )
+  })
 })
--- server/server.js
+++ server/server.js
@@ -23,12 +23,22 @@ app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render('root'))
+app.get('/', (req, res) => res.render('home'))
 
 app.get('/algs', (req, res) => res.render('algs'))
+
+app.post('/algs', (req, res) => {
+  const receivedData = req.body // Получаем данные, отправленные клиентом
+  const responseString = JSON.stringify(receivedData)
+
+  // Отправляем ответ в виде строки
+  res.type('text/plain')
+  res.send(responseString)
+})
 
 app.listen(5000, () => {
   console.log('Сервер запущен, порт 5000')
